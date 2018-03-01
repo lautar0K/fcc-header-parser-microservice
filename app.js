0d02c3860636a595c5f926df60beb21e9bd687d5
lautar0K <laukpo@gmail.com>
2018-03-01 01:41:25 -0300

    Show params

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.end(req.params.toString());
+  console.log(req.params);
+  res.end();
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
