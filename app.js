d786b116df7f36d410cec53334c500b7f62c4780
lautar0K <laukpo@gmail.com>
2018-03-01 01:21:29 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.end("Hello.");
+});
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port ", this.address().port, app.settings.env)
+});
