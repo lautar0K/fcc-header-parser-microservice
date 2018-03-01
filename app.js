bc544a961fe70077943300ac9a52b133327a751c
lautar0K <laukpo@gmail.com>
2018-03-01 02:09:05 -0300

    Show params

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/whoami", async function(req, res) {
-  console.log(req.query);
+  console.log(req.params);
   res.end();
 });
 app.listen(process.env.PORT || 3000, function() {
