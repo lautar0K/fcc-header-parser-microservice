ca9c123a3df1e77242fa284f72391b52a10375bf
lautar0K <laukpo@gmail.com>
2018-03-01 03:37:47 -0300

    Handle / request

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const app = express();
 
-let ready = 0;
-
 app.get("/", function (req, res) {
-  res.redirect("/api/whoami").then(ready += 1);
+  res.redirect("/api/whoami");
 });
 app.get("/api/whoami", function(req, res) {
-  if(ready == 1){
-    let json = new Object;
-    json.ipaddress = req.rawHeaders[23];
-    json.language = req.rawHeaders[17].substr(0, 5);
-    json.software = req.rawHeaders[9].match(/([^(^)])+/g)[1];
-    res.json(json);
-  }
+  let json = new Object;
+  let headers = req.rawHeaders;
+  json.ipaddress = headers[23];
+  json.language = headers[17].substr(0, 5);
+  json.software = headers[9].match(/([^(^)])+/g)[1];
+  res.json(json);
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
