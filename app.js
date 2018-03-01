d2bf1da9024821ff7da10567d3a6efc17a64757d
lautar0K <laukpo@gmail.com>
2018-03-01 02:26:51 -0300

    Unfiltered answer

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/api/whoami", async function(req, res) {
-  console.log(req.rawHeaders[21], req.rawHeaders[17], req.rawHeaders[9]);
-  res.end();
+  let json = new Object;
+  json.ipaddress = req.rawHeaders[21];
+  json.language = req.rawHeaders[17];
+  json.software = req.rawHeaders[9];
+  res.json(json);
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
