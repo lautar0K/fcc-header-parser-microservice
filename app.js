1836bb79dc9c26109f1abbd66048cc0acf9a05fe
lautar0K <laukpo@gmail.com>
2018-03-01 02:21:12 -0300

    Show headers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/whoami", async function(req, res) {
-  console.log(req.rawHeaders);
+  console.log(req.rawHeaders[21], req.rawHeaders[17], req.rawHeaders[9]);
   res.end();
 });
 app.listen(process.env.PORT || 3000, function() {
