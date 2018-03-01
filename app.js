426d86703a5988d7b2e1b12ca6becf2500f67fda
lautar0K <laukpo@gmail.com>
2018-03-01 03:48:18 -0300

    Handle / request

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/api/whoami", function(req, res) {
   json.ipaddress = headers[23];
   json.language = headers[17].substr(0, 5);
   json.software = headers[9].match(/([^(^)])+/g)[1];
-  setTimeout(res.json(json), 3000);
+  setTimeout(res.json(json), 30000);
 });
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port ", this.address().port, app.settings.env)
